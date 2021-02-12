1ec1b40e71b0a092bac05dfc5f632eee48c63310
sunpa <spin2k3@hotmail.com>
2021-02-11 23:14:56 -0800

    End of section 6

--- API/Startup.cs
+++ API/Startup.cs
@@ -27,6 +27,13 @@ namespace API
                 x.UseSqlServer(_config.GetConnectionString("DefaultConnection")));
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });                
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
